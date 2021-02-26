f7e2286de020e3230452315d7d27603c05acaa5a
natfontanesi <natfontanesi@gmail.com>
2021-02-26 19:55:20 -0300

    Add funções e começando a pensar em caminhos

--- jogo.py
+++ jogo.py
@@ -1,9 +1,9 @@
 def jogar():
-    def primeira_decisao():
+    def decisao_casa():
         print("Você está na sua casa, você sai de casa direto pro trabalho(1) ou toma café da manhã antes(2)?")
         
-    def segunda_decisao():
-        print("")
+    def decisao_telefone():
+        print("É a princesa Peaches pedindo ajuda, ela precisa que você leve a sei la o que para ela no hospital. ")
 
     def final():
         print("Fim de jogo deseja jogar novamente? Digite 1 para sim ou qualquer outra tecla para fechar o jogo")
@@ -19,14 +19,17 @@ def jogar():
     print("Bem vindo ao jogo Mário Versus Corona")
     print("**********************************")
 
-    primeira_decisao()
+    decisao_casa()
     decisao = int(input())
     if(decisao == 1):
-        
-    elif(decisao == 2):
+        print("Você está a caminho do trabalho quando seu telefone toca")
+        decisao_telefone()
 
+    elif(decisao == 2):
+        print ("Você está em casa tomando café quando seu telefone toca")
+        decisao_telefone()
     else:
         primeira decisao()
-        
+    print("Você vai ajudar a princesa? (1) ou você vai trabalhar? (2)")   
 
 
