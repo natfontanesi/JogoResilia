1b18c39ca2a39b4d84818db3b0216cf7da933cd6
natfontanesi <natfontanesi@gmail.com>
2021-03-01 20:17:38 -0300

    Colocando as funções em ordem

--- jogo.py
+++ jogo.py
@@ -3,33 +3,41 @@ def jogar():
     print("Bem vindo ao jogo Mário Versus Corona")
     print("**********************************")
 
-    decisao_casa()
-    decisao = int(input())
-    if(decisao == 1):
-        print("Você está a caminho do trabalho quando seu telefone toca")
-        decisao_telefone()
+    decisao = decisao_casa()
+    if(decisao == True):
+        decisao_caminho_trabalho()
 
-    elif(decisao == 2):
+    elif(decisao == False):
         print ("Você está em casa tomando café quando seu telefone toca")
-        decisao_telefone()
-    else:
-        primeira decisao()
+        decisao_café_da_manhã()
+    
 
     print("Você vai ajudar a princesa? (1) ou você vai trabalhar? (2)")   
     
     
 def estrutura_decisoria(decisao):
     if(decisao == 1):
-        return decisão = True
+        return decisao = True
     
     elif(decisao == 2):
         return decisao = False 
 
 def decisao_casa():
-    print("Você está na sua casa, você sai de casa direto pro trabalho(1) ou toma café da manhã antes(2)?")
+    print("Você está na sua casa, pensando no que vem por aí no seu dia, com saudades da emoção que tinha de entrar pelos canos e derrotar tartarugas malignas.")
+    print("No meio do seu devaneio você percebe que não tomou café da manhã e que precisa sair de casa.")
+    decisao=int(input("Você sai de casa imediatamente pro trabalho(1) ou toma café da manhã antes(2): "))
+    if(decisao == 1):
+        decisao = True
+    
+    elif(decisao == 2):
+        decisao = False
+    return decisao
         
-def decisao_telefone():
-    print("É a princesa Peaches pedindo ajuda, ela precisa que você leve a sei la o que para ela no hospital. ")
+def decisao_caminho_trabalho():
+    print("Seu telefone toca e você atende. É a princesa Peaches, ela está te chamando para uma aventura como nos velhos tempos. ")
+    print("Levar os suprimentos para um hospital próximo que está passando por problemas.")
+    print("E aí? Você topa?")
+    print("(1) Sim (2) Não")
 
 def final():
     print("Fim de jogo deseja jogar novamente? Digite 1 para sim ou qualquer outra tecla para fechar o jogo")
